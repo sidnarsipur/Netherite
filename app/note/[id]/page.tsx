d14e4aebac56a3ac7ba0ea991dbf65c476463335
Henry Liu <codicate@gmail.com>
2024-11-16 16:32:00 -0500

    nav actions

--- app/note/[id]/page.tsx
+++ app/note/[id]/page.tsx
@@ -5,6 +5,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { notes, getPageById } from "@/lib/note-manager";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { SearchDialog } from "@/components/search-dialog/search-dialog";
+import NavActions from "@/components/sidebar/nav-actions";
 
 export default async function Page({
   params,
@@ -21,9 +22,13 @@ export default async function Page({
   return (
     <Dialog>
       <div className="flex flex-1 flex-col items-start gap-5 p-10">
-        <div className="flex items-center gap-3">
+        <div className="flex w-full items-center gap-3">
           <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb path={page.path} />
+          <div className="ml-auto px-3">
+            <NavActions />
+          </div>
         </div>
         <div className="w-full rounded-xl border border-border bg-card">
           <h1 className="p-5 text-2xl font-bold">{page.name}</h1>
--- components/note/breadcrumb.tsx
+++ components/note/breadcrumb.tsx
@@ -3,21 +3,12 @@ import { ChevronDown, Slash } from "lucide-react";
 import {
   Breadcrumb,
   BreadcrumbItem,
-  BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export default function BreadcrumbWithDropdown({ path }: { path: String }) {
   const segments = path.split("/");
-  console.log(segments);
   return (
     <Breadcrumb>
       <BreadcrumbList>
