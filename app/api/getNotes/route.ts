9ede59c68dcb438f432f11de12b381079934299b
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2024-11-16 16:47:43 -0500

    GetNotes

---
+++ app/api/getNotes/route.ts
@@ -0,0 +1,25 @@
+import { db, pc } from "@/app/lib/init";
+
+export async function POST(req: Request){
+    try{
+        const { userID } = await req.json();
+
+        const notesSnapshot = await db.collection('notes').where('userID', '==', userID).get();
+
+        if (notesSnapshot.empty) {
+            return Response.json({ message: 'No notes found for this user' }, { status: 404 });
+        }
+
+        const noteIDs: string[] = [];
+        
+        notesSnapshot.forEach(doc => {
+            noteIDs.push(doc.id);
+        });
+
+        return Response.json({ noteIDs });
+    }
+
+    catch(error){
+        return new Response(JSON.stringify({ message: 'Failed to create note', error: error.message }), { status: 500 });
+    }
+}
