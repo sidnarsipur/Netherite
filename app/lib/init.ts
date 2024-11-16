3e423019d173c8f7f01be29332133afffcfd40fe
Henry Liu <codicate@gmail.com>
2024-11-16 18:24:37 -0500

    sidgaz fix

--- app/lib/init.ts
+++ app/lib/init.ts
@@ -8,10 +8,10 @@ var serviceAccount = require("./service_account_key.json");
 
 if(!getApps().length){
     initializeApp({
-        credential: admin.credential.cert(serviceAccount)
+        credential: admin.credential.cert(serviceAccount),
     });
 }
 
 export const auth = getAuth()
 export const db = getFirestore();
-export const pc = new Pinecone({ apiKey: "" });
+export const pc = new Pinecone({ apiKey: "pcsk_MhrCv_Bz9nnEFuBtBAN1NrX8FZfiAYe1j2sMpoNTFcyUAT3NvQXijF847XnmWxsnbXEbz" });
