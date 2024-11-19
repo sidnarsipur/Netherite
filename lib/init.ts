449c4edeb9a21e894fa26e8ea2b00001b3e7e926
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2024-11-18 20:17:59 -0500

    Service Key Fix

--- lib/init.ts
+++ lib/init.ts
@@ -6,17 +6,10 @@ import { createGoogleGenerativeAI } from "@ai-sdk/google";
 
 var admin = require("firebase-admin");
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
+const serviceAccountKey = JSON.parse(process.env.SERVICE_ACCOUNT_KEY || "");
 
 if (!getApps().length) {
-  initializeApp(firebaseConfig);
+  initializeApp(serviceAccountKey);
 }
 
 export const auth = getAuth();
